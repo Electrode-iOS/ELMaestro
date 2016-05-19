f486f3a37c2625a27d12f579cecbb855a1bfb679
Angelo Di Paolo <angelod101@gmail.com>
2016-05-19 17:37:07 -0400

    forward app delegate events along to feature plugins

--- ELMaestro/Supervisor.swift
+++ ELMaestro/Supervisor.swift
@@ -141,8 +141,12 @@ public class Supervisor: UIResponder {
     private var proposedPlugins = [Pluggable]()
     private var loadedPlugins = [Pluggable]()
     public private(set) var startedPlugins = [Pluggable]()
+    
+    /// Get all of the started plugins that conform to PluggableFeature
+    var startedFeaturePlugins: [PluggableFeature] {
+        return startedPlugins.flatMap { $0 as? PluggableFeature }
+    }
 }
-
 
 @objc
 public class ApplicationSupervisor: Supervisor, UIApplicationDelegate {
@@ -170,29 +174,36 @@ public class ApplicationSupervisor: Supervisor, UIApplicationDelegate {
     public var window: UIWindow? = nil
     
     public func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
     
     public func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        for feature in startedFeaturePlugins {
+            feature.applicationWillResignActive?()
+        }
     }
     
     public func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        for feature in startedFeaturePlugins {
+            feature.applicationDidEnterBackground?()
+        }
     }
     
     public func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        for feature in startedFeaturePlugins {
+            feature.applicationWillEnterForeground?()
+        }
     }
     
     public func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        for feature in startedFeaturePlugins {
+            feature.applicationDidBecomeActive?()
+        }
     }
     
     public func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        for feature in startedFeaturePlugins {
+            feature.applicationDidBecomeActive?()
+        }
     }
 }
