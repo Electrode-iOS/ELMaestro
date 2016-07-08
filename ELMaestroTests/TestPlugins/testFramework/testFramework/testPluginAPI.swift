84cabd876532d375eda26a22c510590275638658
Steve Riggins <steve@steveriggins.net>
2016-07-08 11:26:39 -0700

    Refactor sharedInstance; Add Continuity support
    Refactored ApplicationSupervisor.sharedInstance to return the shared application’s app delegate. This allows the ApplicationSupervisor to be instantiated directly for unit testing, and also couples the API to UIApplication, as ApplicationSupervisor is meant to be the app delegate.
    
    Added support for Continuity by extending the PluggableFeature protocol and dispatching to plugins that implement the API.
    
    Added a unit test to test the dispatch mechanism on ApplicationSupervisor. Added a testPluginAPI to capture test results for later inspection by a unit test.
    
    Added the unit test target to the ELMaestro scheme’s test phase.

---
+++ ELMaestroTests/TestPlugins/testFramework/testFramework/testPluginAPI.swift
@@ -0,0 +1,14 @@
+//
+//  testPluginAPI.swift
+//  testFramework
+//
+//  Created by Steve Riggins on 7/8/16.
+//  Copyright © 2016 StereoLab. All rights reserved.
+//
+
+import Foundation
+
+
+public class TestPluginAPI : NSObject {    
+    public var continuityType: String = ""
+}
