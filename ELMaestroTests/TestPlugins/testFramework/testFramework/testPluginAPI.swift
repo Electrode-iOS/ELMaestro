e171bb9d415b23cc25f03f2236716d2c326c060d
Angelo Di Paolo <angelod101@gmail.com>
2017-10-17 14:21:15 -0700

    update unit tests after `ApplicationSupervisor` decouple refactor

--- ELMaestroTests/TestPlugins/testFramework/testFramework/testPluginAPI.swift
+++ ELMaestroTests/TestPlugins/testFramework/testFramework/testPluginAPI.swift
@@ -8,7 +8,12 @@
 
 import Foundation
 
-
 open class TestPluginAPI : NSObject {    
     open var continuityType: String = ""
+}
+
+final class TestDelegate: ApplicationDelegateProxy {
+    override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Swift.Void) -> Bool {
+        return true
+    }
 }
