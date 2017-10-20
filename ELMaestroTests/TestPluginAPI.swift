6d2e4fd9ff321834650b072793b0bf108dbf30da
Angelo Di Paolo <angelod101@gmail.com>
2017-10-20 09:51:20 -0700

    add tests to cover ApplicationDelegateProxy and ApplicationSupervisor

---
+++ ELMaestroTests/TestPluginAPI.swift
@@ -0,0 +1,32 @@
+//
+//  TestPluginAPI.swift
+//  ELMaestro
+//
+//  Created by Angelo Di Paolo on 10/19/17.
+//  Copyright © 2017 WalmartLabs. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+final class TestPluginAPI : NSObject {
+    var continuityType: String = ""
+    
+    var applicationDidFinishLaunchingWithOptionsCalled: XCTestExpectation?
+    var applicationWillResignActiveCalled: XCTestExpectation?
+    var applicationDidEnterBackgroundCalled: XCTestExpectation?
+    var applicationWillEnterForegroundCalled: XCTestExpectation?
+    var applicationDidBecomeActiveCalled: XCTestExpectation?
+    var applicationWillTerminateCalled: XCTestExpectation?
+    var applicationDidReceiveMemoryWarningCalled: XCTestExpectation?
+    var didRegisterUserNotificationSettingsCalled: XCTestExpectation?
+    var handleActionWithIdentifierForLocalNotificationWithResponseInfoCalled: XCTestExpectation?
+    var didRegisterForRemoteNotificationsWithDeviceTokenCalled: XCTestExpectation?
+    var didFailToRegisterForRemoteNotificationsWithErrorCalled: XCTestExpectation?
+    var didReceiveRemoteNotificationCalled: XCTestExpectation?
+    var handleActionWithIdentifierForRemoteNotificationCalled: XCTestExpectation?
+    var handleActionWithIdentifierForLocalNotificationCalled: XCTestExpectation?
+    var handleActionWithIdentifierWithResponseInfoCalled: XCTestExpectation?
+    var performActionForShortcutItemCalled: XCTestExpectation?
+    var didReceiveLocalNotificationCalled: XCTestExpectation?
+}
