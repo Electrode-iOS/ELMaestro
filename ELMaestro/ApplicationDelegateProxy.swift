d5918905d0d9a250772776d128de96f844d9c852
Cihan Cimen <CCimen@walmartlabs.com>
2018-07-05 07:27:39 -0700

    Add a static library target

--- ELMaestro/ApplicationDelegateProxy.swift
+++ ELMaestro/ApplicationDelegateProxy.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 @objc
 open class ApplicationDelegateProxy: UIResponder, UIApplicationDelegate {
--- ELMaestro/ApplicationSupervisor.swift
+++ ELMaestro/ApplicationSupervisor.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 @objc
 public final class ApplicationSupervisor: Supervisor {
