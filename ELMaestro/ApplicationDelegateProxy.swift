7c5ccf25aff5f0f07fa09e955362be924d1dca2d
Angelo Di Paolo <angelod101@gmail.com>
2019-07-16 08:54:17 -0700

    ¯\_(ツ)_/¯

--- ELMaestro/ApplicationDelegateProxy.swift
+++ ELMaestro/ApplicationDelegateProxy.swift
@@ -162,13 +162,15 @@ open class ApplicationDelegateProxy: UIResponder, UIApplicationDelegate {
     open func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
         print("ELMaestro started plugin count \(supervisor.startedFeaturePlugins.count)")
         for feature in supervisor.startedFeaturePlugins {
-            print("feature")
+            print("ELMaestro iterate feature")
             let handled = feature.application?(app, open: url, options: options)
-            if let featureHandled = handled, featureHandled {
-                print("feature handled")
+            print("ELMaestro feature handled")
 
-                return true
-            }
+//            if let featureHandled = handled, featureHandled {
+//                print("feature handled")
+//
+//                return true
+//            }
         }
 
         return false
